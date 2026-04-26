580ca284996b4ba15470788b17724e0be5b941bd
Pranshi Patel <patelpranshi88@gmail.com>
2026-04-26 12:08:42 +0530

    [Pranshi Patel] : added list service

--- list-service/src/main/java/com/flowboard/list/dto/CreateListRequest.java
+++ list-service/src/main/java/com/flowboard/list/dto/CreateListRequest.java
@@ -1,5 +1,25 @@
 package com.flowboard.list.dto;
 
-public class ggg {
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateListRequest {
+
+    @NotNull(message = "Board ID is required")
+    private Long boardId;
+
+    @NotBlank(message = "List name is required")
+    @Size(max = 100, message = "Name must not exceed 100 characters")
+    private String name;
+
+    @PositiveOrZero(message = "Position must be 0 or greater")
+    private Integer position;
+
+    @Size(max = 20, message = "Color value is too long")
+    private String color;
 }
